ed3ce541f8eaf74ed398eae9e72e3b5302326cfb
LICQ03 <LICQ03@stpiusx.nsw.edu.au>
2022-05-29 20:12:33 +1000

    Finished though not finished all content

--- src/Cores/LoginCore.js
+++ src/Cores/LoginCore.js
@@ -29,7 +29,7 @@ export const LoginCore = () => {
     }
     function buttonClickSubmit(){
 
-        axios.post("http://ipt-api.herokuapp.com/users/loginIdentifier", userInputData).then(
+        axios.post("https://ipt-api.herokuapp.com/users/loginIdentifier", userInputData).then(
             function (res){
                 if(res.data.completion) {
                     console.log("correct input")
